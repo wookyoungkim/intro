575f20ba8db895414dbcda943e7952b385be41d6
wookyoungkim <viliketh1s98@naver.com>
2020-06-21 04:18:29 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+//import logo from './logo.svg';
 import './App.css';
+import SiderDemo from './component/SiderDemo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <SiderDemo></SiderDemo>
+    );
+  }
 }
 
 export default App;
